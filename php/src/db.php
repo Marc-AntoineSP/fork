3633735b1d909910dc1322800fdd127b885d6324
MSKTArcanine <marcantoine1999@live.fr>
2025-10-09 21:05:23 +0200

    FEAT: transformation en classe pour import : connextion db

--- php/src/db.php
+++ php/src/db.php
@@ -17,3 +17,24 @@ function pdo_sql():PDO{
     ]);
     return $pdo;
 }
+
+final class Connection {
+    public static function connnect():PDO {
+        $host = getenv('PMA') ?: '127.0.0.1';
+        $port = (int)getenv('PMAPORT') ?: 3306;
+        $db = getenv('PMADB') ?:'projet_cube2';
+        $user = getenv('PMAUSER') ?:'app';
+        $pass = getenv('PMAPASS') ?:'app_password';
+
+        return new PDO(
+            "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
+            $user,
+            $pass,
+            [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES => false,
+            ]
+        );
+    }
+}
